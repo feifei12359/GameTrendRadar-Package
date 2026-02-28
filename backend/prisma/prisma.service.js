426f36324a16d835c250d77ae92d7d56e88a94cc
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-02-28 20:25:06 +0800

    Add files via upload

--- backend/prisma/prisma.service.js
+++ backend/prisma/prisma.service.js
@@ -8,13 +8,57 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PrismaService = void 0;
 const common_1 = require("@nestjs/common");
-const client_1 = require("@prisma/client");
-let PrismaService = class PrismaService extends client_1.PrismaClient {
+let PrismaService = class PrismaService {
+    constructor() {
+        // 使用内存存储作为后备方案
+        this.games = [];
+        this.nextId = 1;
+    }
     async onModuleInit() {
-        await this.$connect();
+        console.log('Using in-memory storage for games');
     }
     async onModuleDestroy() {
-        await this.$disconnect();
+        // 清理资源
+    }
+    // 模拟Prisma Client的方法
+    async game() {
+        return {
+            findMany: async (options) => {
+                let result = [...this.games];
+                if (options && options.where && options.where.totalScore) {
+                    result = result.filter(game => game.totalScore >= options.where.totalScore.gte);
+                }
+                if (options && options.orderBy && options.orderBy.totalScore) {
+                    result.sort((a, b) => {
+                        return options.orderBy.totalScore === 'desc' ? b.totalScore - a.totalScore : a.totalScore - b.totalScore;
+                    });
+                }
+                return result;
+            },
+            create: async (options) => {
+                const newGame = {
+                    id: this.nextId++,
+                    ...options.data,
+                    createdAt: new Date()
+                };
+                this.games.push(newGame);
+                return newGame;
+            },
+            findUnique: async (options) => {
+                return this.games.find(game => game.url === options.where.url) || null;
+            },
+            update: async (options) => {
+                const index = this.games.findIndex(game => game.id === options.where.id);
+                if (index !== -1) {
+                    this.games[index] = {
+                        ...this.games[index],
+                        ...options.data
+                    };
+                    return this.games[index];
+                }
+                return null;
+            }
+        };
     }
 };
 exports.PrismaService = PrismaService;

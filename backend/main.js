bca67d3ffa115e58912214a5619cd64912c8ee1b
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-02-28 20:35:57 +0800

    Add files via upload

--- backend/main.js
+++ backend/main.js
@@ -1,17 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const platform_express_1 = require("@nestjs/platform-express");
 const app_module_1 = require("./app.module");
+let app;
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({
-        origin: ['http://localhost:5173', 'https://game-trend-radar.vercel.app', 'https://game-trend-radar-backend.vercel.app'],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-    });
-    app.setGlobalPrefix('api');
-    const port = process.env.PORT || 4000;
-    await app.listen(port);
-    console.log(`Application is running on: ${await app.getUrl()}`);
+    if (!app) {
+        app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter());
+        app.enableCors({
+            origin: ['http://localhost:5173', 'https://game-trend-radar.vercel.app', 'https://game-trend-radar-backend.vercel.app'],
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            credentials: true,
+        });
+        app.setGlobalPrefix('api');
+        await app.init();
+    }
+    return app;
 }
-bootstrap();
+async function handler(req, res) {
+    const nestApp = await bootstrap();
+    nestApp.getHttpAdapter().getInstance()(req, res);
+}
+exports.handler = handler;
